157683d5718dcf45f4ddc245c9bd6730bbccca01
hs-hack <hs2520390@gmail.com>
2024-02-19 15:41:22 -0700

    organised code

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,18 @@
 export const cart = [];
+
+export function addToCart(productId) {
+  let match;
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      match = cartItem;
+    }
+  });
+  if (match) {
+    match.quantity += 1;
+  } else {
+    cart.push({
+      productId: productId,
+      quantity: 1,
+    });
+  }
+}
