8bbc85dab0e78ce7d3230cba243231936720dfac
hs-hack <hs2520390@gmail.com>
2024-02-19 16:44:13 -0700

    update checkout1.0

--- javascript/checkout.js
+++ javascript/checkout.js
@@ -1,7 +1,8 @@
 import { cart } from "../data/cart.js";
 import { products } from "../data/products.js";
-
+import { formatCurrency } from "./utils/money.js";
 let html = "";
+let i = 0;
 cart.forEach((cartItem) => {
   const productId = cartItem.productId;
   let matchingProduct;
@@ -24,7 +25,7 @@ cart.forEach((cartItem) => {
                   ${matchingProduct.name}
                 </div>
                 <div class="product-price">
-                 $${(matchingProduct.priceCents / 100).toFixed(2)}
+                 $${formatCurrency(matchingProduct.priceCents)}
                 </div>
                 <div class="product-quantity">
                   <span>
@@ -48,7 +49,7 @@ cart.forEach((cartItem) => {
                 <div class="delivery-option">
                   <input type="radio" checked
                     class="delivery-option-input"
-                    name="delivery-option-1">
+                    name="delivery-option-${i}">
                   <div>
                     <div class="delivery-option-date">
                       Tuesday, June 21
@@ -61,7 +62,7 @@ cart.forEach((cartItem) => {
                 <div class="delivery-option">
                   <input type="radio"
                     class="delivery-option-input"
-                    name="delivery-option-1">
+                    name="delivery-option-${i}">
                   <div>
                     <div class="delivery-option-date">
                       Wednesday, June 15
@@ -74,7 +75,7 @@ cart.forEach((cartItem) => {
                 <div class="delivery-option">
                   <input type="radio"
                     class="delivery-option-input"
-                    name="delivery-option-1">
+                    name="delivery-option-${i}">
                   <div>
                     <div class="delivery-option-date">
                       Monday, June 13
@@ -88,6 +89,8 @@ cart.forEach((cartItem) => {
             </div>
           </div>
     `;
+    i++
+
 });
 
 document.querySelector(".js-order-summary").innerHTML = html;
