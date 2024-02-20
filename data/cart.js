cb9588d978a0ee3d7de08123414ae77b6c152155
hs-hack <hs2520390@gmail.com>
2024-02-19 17:18:53 -0700

    added localstorage

--- data/cart.js
+++ data/cart.js
@@ -1,13 +1,12 @@
-export let cart = [
-  {
-    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6", //socks
-    quantity: 2,
-  },
-  {
-    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",//basketball
-    quantity: 1,
-  },
-];
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+if (!cart) {
+    cart = []
+}
+
+function storage() {
+  localStorage.setItem("cart", JSON.stringify(cart));
+}
 
 export function addToCart(productId) {
   let match;
@@ -23,15 +22,17 @@ export function addToCart(productId) {
       productId: productId,
       quantity: 1,
     });
-  }
+    }
+    storage();
 }
 
 export function removeFromCart(productId) {
-    const newCart = []
-    cart.forEach((cartItem) => {
-        if (cartItem.productId !== productId) {
-            newCart.push(cartItem)
-        }
-    })
-    cart=newCart
+  const newCart = [];
+  cart.forEach((cartItem) => {
+    if (cartItem.productId !== productId) {
+      newCart.push(cartItem);
+    }
+  });
+    cart = newCart;
+    storage();
 }
--- javascript/checkout.js
+++ javascript/checkout.js
@@ -1,7 +1,7 @@
 import { cart, removeFromCart } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
-
+console.log(cart)
 let html = "";
 let i = 0;
 cart.forEach((cartItem) => {
